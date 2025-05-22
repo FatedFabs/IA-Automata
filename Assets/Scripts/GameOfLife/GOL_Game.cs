b1a36dbe3bf19b55855400a8f3300fc5c7b8ab38
FatedFabs <fabiansanchezgarcia13@outlook.com>
2025-05-22 11:30:30 -0600

    GOL complete
    - Added logic of game

--- Assets/Scripts/GameOfLife/GOL_Game.cs
+++ Assets/Scripts/GameOfLife/GOL_Game.cs
@@ -8,16 +8,16 @@ public class GOL_Game : MonoBehaviour
     [SerializeField] private Tilemap currentTilemap;
     [SerializeField] private Tilemap nextTilemap;
     [SerializeField] private Tile cellTile;
+    [SerializeField] private float updateTimer = 0.1f;
 
-    [SerializeField] private GOL_Pattern pattern;
-
-    [SerializeField] private float updateTimer = 0.05f;
-
+    private HashSet<Vector3Int> aliveCells;
+    private HashSet<Vector3Int> cellsToCheck;
     private bool gameStart = false;
 
-    void Start()
+    private void Awake()
     {
-        //SetInitialPattern(pattern);
+        aliveCells = new HashSet<Vector3Int>();
+        cellsToCheck = new HashSet<Vector3Int>();
     }
 
     private void Update()
@@ -34,30 +34,111 @@ public class GOL_Game : MonoBehaviour
     private void PaintCell(Vector3Int _pos)
     {
         if (!currentTilemap.HasTile(_pos))
+        {
             currentTilemap.SetTile(_pos, cellTile);
+            aliveCells.Add(_pos);
+        }
         else
+        {
             currentTilemap.SetTile(_pos, null);
+            aliveCells.Remove(_pos);
+        }
     }
 
     public void Play()
     {
-        gameStart = true;
-
-        nextTilemap = currentTilemap;
-        currentTilemap.ClearAllTiles();
+        StartCoroutine(Simulate());
     }
 
-    private void SetInitialPattern(GOL_Pattern _pattern)
+    public void Stop()
     {
+        StopAllCoroutines();
+    }
+
+    public void Reset()
+    {
+        Stop();
         ClearTilemap();
+        aliveCells.Clear();
+        cellsToCheck.Clear();
+    }
 
-        Vector2Int center = pattern.GetCenter();
-
-        for (int i = 0; i < pattern.cells.Length; i++)
+    private IEnumerator Simulate()
+    {
+        while (enabled)
         {
-            Vector2Int cell = pattern.cells[i] - center;
-            currentTilemap.SetTile((Vector3Int)cell, cellTile);
+            UpdateState();
+            yield return new WaitForSeconds(updateTimer);
         }
+    }
+
+    private void UpdateState()
+    {
+        //Checks for cells in adjacent tiles
+        cellsToCheck.Clear();
+
+        foreach (var cell in aliveCells)
+        {
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    cellsToCheck.Add(cell + new Vector3Int(x, y, 0));
+                }
+            }
+        }
+
+        //Set tiles to the next state
+        foreach (var cell in cellsToCheck)
+        {
+            int neighbors = countNeighbors(cell);
+            bool alive = isAlive(cell);
+
+            if(!alive &&  neighbors == 3)
+            {
+                nextTilemap.SetTile(cell, cellTile);
+                aliveCells.Add(cell);
+            }
+            else if(alive && neighbors < 2 || neighbors > 3)
+            {
+                nextTilemap.SetTile(cell, null);
+                aliveCells.Remove(cell);
+            }
+            else
+            {
+                nextTilemap.SetTile(cell, currentTilemap.GetTile(cell));
+            }
+        }
+
+        Tilemap temp = currentTilemap;
+        currentTilemap = nextTilemap;
+        nextTilemap = temp;
+        nextTilemap.ClearAllTiles();
+    }
+
+    private int countNeighbors(Vector3Int cell)
+    {
+        int count = 0;
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                Vector3Int neighbor = cell + new Vector3Int(x, y, 0);
+
+                if(x == 0 && y == 0)
+                    continue;
+                else if(isAlive(neighbor))
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    private bool isAlive(Vector3Int cell)
+    {
+        return currentTilemap.HasTile(cell);
     }
 
     private void ClearTilemap()
